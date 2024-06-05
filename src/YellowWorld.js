7dff8121169c1623ee1b8ff2d4af74f261460ecd
Tilek <tcherikbaev@na.edu>
2024-06-05 14:37:17 -0500

    Yellow World

---
+++ src/YellowWorld.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const YellowWorld = () => {
+  const yellowStyle = {
+    backgroundColor: 'yellow',
+    padding: '20px',
+    textAlign: 'center'
+  };
+
+  return (
+    <div style={yellowStyle}>
+      Yellow World
+    </div>
+  );
+};
+
+export default YellowWorld;
